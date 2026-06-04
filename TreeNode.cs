7d1e49f7d35407af410436437be97c072111ddf6
Karl Ola Jesper Sten <h24ksten@du.se>
2026-06-04 15:56:56 +0200

    created TreeNode class

---
+++ TreeNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labb_7
+{
+    public class TreeNode
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+
+        public TreeNode FirstChild { get; set; }
+        public TreeNode NextSibling { get; set; }
+
+        public TreeNode(string id, string type, string name)
+        {
+            Id = id;
+            Type = type;
+            Name = name;
+        }
+    }
+}
